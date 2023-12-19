463e5704ddca572e2575619992384dd9ed66ca3a
cHoObie <chumanov.vadim3@gmail.com>
2023-12-19 09:46:58 +0300

    Task 1

--- Homework_Lesson3_Arrays/Program.cs
+++ Homework_Lesson3_Arrays/Program.cs
@@ -1,6 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел 
+//от 1 до 100. Найдите количество элементов массива, 
+//значения которых лежат в отрезке [20,90].
+
+Console.Clear();
+
+int[] arr = new int [10];
+            Random random = new Random();
+            int count = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = random.Next(1, 101);
+                if (arr[i] >= 20 && arr[i] <= 90)
+                {
+                    count++;
+                }
+            }
+
+Console.WriteLine(String.Join(", ", arr));
+Console.WriteLine(count);
