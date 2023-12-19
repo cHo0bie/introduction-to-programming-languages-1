f8cced02aee9272a904eafcc3ab1c85afe357875
cHoObie <chumanov.vadim3@gmail.com>
2023-12-19 10:00:44 +0300

    Task 2

--- Homework_Lesson3_Arrays/Program.cs
+++ Homework_Lesson3_Arrays/Program.cs
@@ -2,20 +2,59 @@
 //от 1 до 100. Найдите количество элементов массива, 
 //значения которых лежат в отрезке [20,90].
 
-Console.Clear();
+// Console.Clear();
 
-int[] arr = new int [10];
+// int[] arr = new int [10];
+//             Random random = new Random();
+//             int count = 0;
+
+//             for (int i = 0; i < 10; i++)
+//             {
+//                 arr[i] = random.Next(1, 101);
+//                 if (arr[i] >= 20 && arr[i] <= 90)
+//                 {
+//                     count++;
+//                 }
+//             }
+
+// Console.WriteLine(String.Join(", ", arr));
+// Console.WriteLine(count);
+
+//==========================================================
+
+//Задача 2: Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет количество 
+// чётных чисел в массиве.
+
+namespace EvenNumberInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int [10];
             Random random = new Random();
             int count = 0;
-
             for (int i = 0; i < 10; i++)
             {
-                arr[i] = random.Next(1, 101);
-                if (arr[i] >= 20 && arr[i] <= 90)
+                array[i] = random.Next(1, 101);
+                if (array[i] >= 20 && array[i] <= 90)
                 {
                     count++;
                 }
             }
+Console.WriteLine(String.Join(", ", array));
+            int evenCount = 0;
 
-Console.WriteLine(String.Join(", ", arr));
-Console.WriteLine(count);
+            foreach (int number in array)
+            {
+                if (number % 2 == 0)
+                {
+                    evenCount++;
+                }
+            }
+
+Console.WriteLine($"Колличество четных чисел:  {evenCount}");
+        }
+    }
+}
