e0388aba455d1f51bfc09145aac037d02f4c01ce
cHoObie <chumanov.vadim3@gmail.com>
2023-12-27 01:20:33 +0300

    Task 3

--- Homework_Leson7_Recursion/Program.cs
+++ Homework_Leson7_Recursion/Program.cs
@@ -2,24 +2,51 @@
 // выведет все натуральные числа в промежутке от M до N.
 // Использовать рекурсию, не использовать циклы.
 
+// class Program
+// {
+//     static void Main()
+//     {
+//         int num_M = 1;
+//         int num_N = 100;
+
+//         PrintNums(num_M, num_N);
+//     }
+
+//     static void PrintNums(int num_M, int num_N)
+//     {
+//         if (num_M > num_N)
+//             return;
+        
+//         Console.WriteLine(num_M);
+//         PrintNums(num_M + 1, num_N);
+//     }
+// }
+
+//===========================================================
+
+// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. Использовать 
+// рекурсию, не использовать циклы.
+
+
+using System;
+
 class Program
 {
     static void Main()
     {
-        int num_M = 1;
-        int num_N = 100;
-
-        PrintNums(num_M, num_N);
+        int[] array = { 1, 2, 3, 4, 5 };
+        PrintReverse(array, array.Length - 1);
     }
 
-    static void PrintNums(int num_M, int num_N)
+    static void PrintReverse(int[] array, int index)
     {
-        if (num_M > num_N)
+        if (index < 0)
+        {
             return;
-        
-        Console.WriteLine(num_M);
-        PrintNums(num_M + 1, num_N);
-    }
-}
+        }
 
-//===========================================================
+        Console.WriteLine(array[index]);
+        PrintReverse(array, index - 1);
+    }
+} 
