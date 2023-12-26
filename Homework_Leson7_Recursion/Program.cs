ee432a97ed02f72eb9d6e330c5e3fd9a9762476e
cHoObie <chumanov.vadim3@gmail.com>
2023-12-27 00:57:23 +0300

    Task 1

---
+++ Homework_Leson7_Recursion/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая 
+// выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+class Program
+{
+    static void Main()
+    {
+        int num_M = 1;
+        int num_N = 100;
+
+        PrintNums(num_M, num_N);
+    }
+
+    static void PrintNums(int num_M, int num_N)
+    {
+        if (num_M > num_N)
+            return;
+        
+        Console.WriteLine(num_M);
+        PrintNums(num_M + 1, num_N);
+    }
+}
+
+//===========================================================
